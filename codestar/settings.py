61152bcb9d8c80ae1c1c4eba82bf63e6797f46b0
Stojj2 <stojj2@hotmail.com>
2023-07-07 19:57:16 +0000

    Deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,6 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ["django-blog-01-83f447e5696e.herokuapp.com", "localhost", "8000-stojj2-djangoblog-l1ybtmg170a.ws-eu101.gitpod.io"]
 
 
